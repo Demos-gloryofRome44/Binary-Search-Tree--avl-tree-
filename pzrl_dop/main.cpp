9af422d41f955b72f6df8d6706fe7b9701b103db
Demos <166336733+Demos-gloryofRome44@users.noreply.github.com>
2024-05-07 18:40:37 +0300

    Initial Commit

---
+++ pzrl_dop/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pzrl_dop
+//
+//  Created by Егор on 07.05.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
